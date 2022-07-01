0389600e439a9b2cdc4e13c889260493418c04d9
ViktoriaZinyak <viktoriazinyak@gmail.com>
2022-07-01 10:47:42 +0300

    modal-mob

---
+++ src/js/header-button-2.js
@@ -0,0 +1,15 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const bodyScroll = document.querySelector('body');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-opn');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    bodyScroll.classList.toggle('no-scroll');
+    mobileMenuRef.classList.toggle('is-opn');
+  });
+})();
