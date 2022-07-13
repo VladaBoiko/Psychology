fdf447553808dae214dfea46fc5c55ad380cf035
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-07-13 13:28:00 +0000

    Deploying to gh-pages from @ VladaBoiko/Psychology@85caadaafd24e772cac4694cab77c8ddb13393cf 🚀

---
+++ payment.713975f8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelectorAll(".header-nav-link"),e=document.location.pathname,n="nav-item-link-current";t.forEach((function(t){t.classList.contains(n)&&t.classList.remove(n),e.includes(t.getAttribute("href"))&&t.classList.add(n)}))}();
+//# sourceMappingURL=payment.713975f8.js.map
