94500a4004b761505fa29d4bd1515033036aa4fe
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-07-13 19:06:50 +0000

    Deploying to gh-pages from @ VladaBoiko/Psychology@79c4b61b41a8a7c0f75df270e1a7ba0a8a6d6637 🚀

---
+++ payment.37790088.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelectorAll(".header-nav-link"),e=document.location.pathname,n="nav-item-link-current";t.forEach((function(t){t.classList.contains(n)&&t.classList.remove(n),e.includes(t.getAttribute("href"))&&t.classList.add(n)})),"/"===e&&t[0].classList.add(n)}();
+//# sourceMappingURL=payment.37790088.js.map
--- payment.38179d87.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll(".header-nav-link"),t=document.location.pathname;e.forEach((e=>{e.classList.contains("nav-item-link-current")&&e.classList.remove("nav-item-link-current"),t.includes(e.getAttribute("href"))&&e.classList.add("nav-item-link-current")}));
-//# sourceMappingURL=payment.38179d87.js.map
